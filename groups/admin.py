bad2b9ec6b34a4b99c9728e72c60e794cd4c2c31
chengshunzhang <chengshun.zhang@outlook.com>
2019-01-08 12:55:43 -0800

    connect views in urls and register models in admin

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from groups import models
 # Register your models here.
+
+class GroupMemberInline(admin.TabularInline):
+    model = GroupMember
+
+admin.site.register(models.Group)
